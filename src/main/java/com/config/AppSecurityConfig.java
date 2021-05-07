e310cad66fdf5b0a3d212866f14c159de1dd170f
RasagnaGade <rasagnagade30@gmail.com>
2021-05-07 18:49:46 +0530

    first commit

---
+++ src/main/java/com/config/AppSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+//import java.util.ArrayList;
+//import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+
+@EnableWebSecurity
+@Configuration
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	@Bean
+	public AuthenticationProvider authProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		return provider;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	@Bean
+//	protected UserDetailsService userDetailsService() {
+//		List<UserDetails> users = new ArrayList<UserDetails>();
+//		users.add(User.withDefaultPasswordEncoder().username("phani").password("1234").roles("USER").build());
+//		return new InMemoryUserDetailsManager(users);
+//	}
+}
---
+++ src/main/java/com/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/ojas/emps")
+public class HelloController {
+	@GetMapping("/sayHello")
+	@ResponseBody
+	public String sayHello() {
+		return "hello ojas";
+	}
+}
